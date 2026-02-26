a8f17ce226ae6b4abc06fd44287761a547620303
Olin Gilster <olin.gilster@icloud.com>
2026-02-26 14:42:49 -0500

    added basic formatting to files

---
+++ ogar.c
@@ -0,0 +1,7 @@
+/*
+Created by Olin Gilster S'26
+This work is subject to the MIT License (https://opensource.org/licenses/MIT)
+*/
+
+// import statements
+#include "ogar.h"
---
+++ ogar.h
@@ -0,0 +1,5 @@
+/*
+Created by Olin Gilster S'26
+This work is subject to the MIT License (https://opensource.org/licenses/MIT)
+*/
+
