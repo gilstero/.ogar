5db64b739c3ebe91dbe1798d177e4730620a6b76
Olin Gilster <olin.gilster@icloud.com>
2026-02-26 14:47:34 -0500

    main loop and import statements

--- ogar_utils.c
+++ ogar_utils.c
@@ -3,5 +3,22 @@ Created by Olin Gilster S'26
 This work is subject to the MIT License (https://opensource.org/licenses/MIT)
 */
 
-// import statements
+// import statements for files
 #include "ogar.h"
+
+// import statements for libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+// get opt
+#include <getopt.h>
+#include <unistd.h>
+
+
+
